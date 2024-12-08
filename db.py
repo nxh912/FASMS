7bebcf5284e8bf377e4f0dd2344302d6fc4de2c8
Ngai Lam Ho <nxh912@gmail.com>
2024-12-09 03:01:22 +0800

    database and data insertion script

--- db.py
+++ db.py
@@ -10,71 +10,59 @@ def create_table(db_file):
 
     ### TABLE : applicants
     try:
-        cursor.execute("""CREATE TABLE IF NOT EXISTS  schemes (
-            id UUID PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            criteria JSONB, 
-            last_update DATETIME DEFAULT CURRENT_TIMESTAMP)
-        """)
-                
-        cursor.execute('''CREATE TABLE IF NOT EXISTS  applicants (
-                id UUID PRIMARY KEY,
-                name VARCHAR(255),
-                employment_status VARCHAR(10),
-                sex VARCHAR(10),
-                date_of_birth DATETIME,
-                last_update DATETIME DEFAULT CURRENT_TIMESTAMP
-        );''')
+        cursor.execute("CREATE TABLE IF NOT EXISTS \
+                        schemes ( id UUID           PRIMARY KEY,\
+                                  Scheme_Name       VARCHAR(255), \
+                                  Marital_Status    VARCHAR(255), \
+                                  Employment_Status VARCHAR(255), \
+                                  Household_Size Int,             \
+                                  last_update DATETIME DEFAULT CURRENT_TIMESTAMP); ")
+        print("TABLE : schemes CREATED")
 
+        # family scheme
+        cursor.execute("""INSERT INTO schemes( name, marital_status, employment_status, household_number) 
+            VALUES
+               ('Single Parent Scheme',       'Widowed|Divorced', 'Employed|Unemployed' , 2),
+               ('No Employment Family Scheme','Married',          'Unemployed',           2);
+        """)
 
         cursor.execute("""CREATE TABLE IF NOT EXISTS  household_person (
             id UUID PRIMARY KEY, 
-            applicant_id UUID NOT NULL, 
+            applicant_id UUID PRIMARY KEY, 
             name VARCHAR(255) NOT NULL, 
             address VARCHAR(255) NOT NULL,
             income INT NOT NULL,
             last_update DATETIME DEFAULT CURRENT_TIMESTAMP, 
             FOREIGN KEY (applicant_id) REFERENCES applicants(id) ON DELETE CASCADE
         );""")
+        print("TABLE : household_person CREATED")
 
         cursor.execute("""CREATE TABLE IF NOT EXISTS  benefits (
             id UUID PRIMARY KEY,
-            scheme_id UUID NOT NULL, 
+            scheme_id UUID PRIMARY KEY, 
             name VARCHAR(255) NOT NULL, 
             amount DECIMAL(10, 2) NOT NULL); 
                        """)
+        print("TABLE : benefits CREATED")
 
-        ## INSERT data into DB
-        ## APPLICATION
-        insert1="INSERT INTO applicants (id, name, employment_status, sex, date_of_birth)" +\
-            " VALUES " +\
-            "('01913b7a-4493-74b2-93f8-e684c4ca935c', 'James', 'unemployed', 'male', '1990-07-01'), "+\
-            "('01913b80-2c04-7f9d-86a4-497ef68cb3a0', 'Mary', 'unemployed', 'female', '1984-10-06'); "    
-        cursor.execute(insert1)
+        cursor.execute('''CREATE TABLE IF NOT EXISTS  applicants (
+                id UUID PRIMARY KEY,
+                name VARCHAR(255) PRIMARY KEY,
+                employment_status VARCHAR(10),
+                sex VARCHAR(10),
+                date_of_birth DATETIME PRIMARY KEY,
+                household_id UUID DEFAULT NULL,
+                last_update DATETIME DEFAULT CURRENT_TIMESTAMP
+        );''')
+        print("TABLE : applicants CREATED")
 
-
-
-        insert2='''INSERT INTO household (id, applicant_id, name, employment_status, sex, date_of_birth, relation)
-        VALUES
-        ('01913b88-1d4d-7152-a7ce-75796a2e8ecf', '01913b80-2c04-7f9d-86a4-497ef68cb3a0', 'Gwen', 'unemployed', 'female', '2016-02-01', 'daughter'),
-        ('01913b88-65c6-7255-820f-9c4dd1e5ce79', '01913b80-2c04-7f9d-86a4-497ef68cb3a0', 'Jayden', 'unemployed', 'male', '2018-03-15', 'son');
-        '''
-        cursor.execute(insert2)
-        insert3='''
-        INSERT INTO schemes (id, name, criteria)
-        VALUES
-        ('01913b89-9a43-7163-8757-01cc254783f3', 'Retrenchment Assistance Scheme', 
-        '{"employment_status": "unemployed"}'),
-        ('01913b89-befc-7ae3-bb37-3079aa7f1be0', 'Retrenchment Assistance Scheme (families)', 
-        '{"employment_status": "unemployed", "has_children": {"school_level": "primary"}}');
-        '''
-        cursor.execute(insert3)
-        insert4='''
-        INSERT INTO benefits (id, scheme_id, name, amount)
-        VALUES
-        ('01913b8b-9b12-7d2c-a1fa-ea613b802ebc', '01913b89-9a43-7163-8757-01cc254783f3', 'SkillsFuture Credits', 500.00);
-        '''
-        cursor.execute(insert4)
+        cursor.execute("""CREATE TABLE IF NOT EXISTS  applications (
+            id UUID PRIMARY KEY,
+            scheme_id UUID    PRIMARY KEY, 
+            applicant_id UUID PRIMARY KEY, 
+            last_update DATETIME DEFAULT CURRENT_TIMESTAMP
+                       )""")
+        print("# TABLE : applications CREATED")
 
     except sqlite3.OperationalError as e:
         if "table applicants already exists" in str(e):
---
+++ insert_samples.sh
@@ -0,0 +1,53 @@
+
+#  "('01913b7a-4493-74b2-93f8-e684c4ca935c', 'James', 'unemployed', 'male', '1990-07-01'), 
+#  "('01913b80-2c04-7f9d-86a4-497ef68cb3a0', 'Mary', 'unemployed', 'female', '1984-10-06'); "    
+
+
+########### applicants / POST
+printf "#####\n# POST applicants \n\n"
+
+ENDPOINT='http://localhost:8000/api/applicants'  
+printf "ENDPOINT : %s\n" $( echo $ENDPOINT | sed s/'.*api'/''/g )
+ 
+#curl --header "Content-Type: application/json" --request POST --data '{"date_of_birth":"1990-07-01", "sex":"Male",   "name":"James", "employment_status":"unemployed"}'  'http://localhost:8000/api/applicants'  
+#curl --header "Content-Type: application/json" --request POST --data '{"date_of_birth":"1990-07-01", "sex":"Female", "name":"Mary",  "employment_status":"unemployed"}'  'http://localhost:8000/api/applicants'  
+
+JAMES_ID=` \
+curl --data '{"date_of_birth":"1990-07-01", "sex":"Male",   "name":"James", "employment_status":"unemployed"}' --header "Content-Type: application/json" --request POST "$ENDPOINT" \
+ | tr '\n' ' ' | sed s/'.*applicant'/''/g | cut -f3 -d\" `
+
+echo "  ----> JAMES_UUID : ${JAMES_ID}"
+
+MARY_CHILD1=` \
+curl --data '{"date_of_birth":"2006-10-06", "sex":"Female", "name":"Nancy",  "employment_status":"unemployed"}' --header "Content-Type: application/json" --request POST "$ENDPOINT" \
+ | tr '\n' ' ' | sed s/'.*applicant'/''/g | cut -f3 -d\" `
+
+MARY_CHILD2=` \
+curl --data '{"date_of_birth":"2008-10-06", "sex":"Male", "name":"Oliver",  "employment_status":"unemployed"}' --header "Content-Type: application/json" --request POST "$ENDPOINT" \
+ | tr '\n' ' ' | sed s/'.*applicant'/''/g | cut -f3 -d\" `
+
+## Nancy {"Action":"Added","applicant":"e3b9f1b2-9bf4-4234-bf87-6692394e2d70","line":54}
+## Oliver {"Action":"Added","applicant":"0ace2660-9871-4f7e-bd25-dbdc7f24ceeb","line":54}
+
+
+
+MARY_ID=` \
+curl --data '{"date_of_birth":"1984-10-06", "sex":"Female", "name":"Mary",  "employment_status":"unemployed"}' --header "Content-Type: application/json" --request POST "$ENDPOINT" \
+ | tr '\n' ' ' | sed s/'.*applicant'/''/g | cut -f3 -d\" `
+
+curl --data '{"date_of_birth":"1984-10-06", "sex":"Female", "name":"Mary",  "employment_status":"unemployed"}' --header "Content-Type: application/json" --request POST "$ENDPOINT"
+
+
+
+
+### sqlite> CREATE TABLE IF NOT EXISTS  schemes ( id UUID PRIMARY KEY, name VARCHAR(255), marital_status VARCHAR(255), employment_status VARCHAR(255), household_number Int, last_update DATETIME DEFAULT CURRENT_TIMESTAMP);
+### sqlite> INSERT INTO schemes( name, marital_status, employment_status, household_number) VALUES ('Single Parent Scheme',     'Widowed|Divorced', 'Employed|Unemployed' , 2), ('No Employment Family Scheme','Married', 'Unemployed', 2);
+
+
+exit
+
+echo "  ----> MARY_UUID : ${MARY_ID}"
+
+########### applicants / GET
+printf "####\n#  GET applicants \n\n"
+curl "$ENDPOINT" 
